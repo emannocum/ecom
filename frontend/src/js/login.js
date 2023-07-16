8927e5206b0d72a3257558fcfc6276e52962c99b
Emmanuel Nocum <emmanuel.nocum@cvsu.edu.ph>
2023-07-16 18:13:35 +0800

    added login

---
+++ frontend/src/js/login.js
@@ -0,0 +1,24 @@
+const FormLogin = document.querySelector('#FrmLogin')
+
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+
+FormLogin.addEventListener('submit', async (event) =>{
+    event.preventDefault()
+
+    const data = {'email': email.value , password: password.value}
+    
+    const request = await fetch ('http://localhost:3000/authenticate', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    })
+
+    const response = await request.json()
+    console.log(response)
+    if(response.auth === true){
+        localStorage.setItem('sessionID', email.value)
+    }else{
+        toastr.error('Login Failed', 'Wrong email or password!')
+    }
+})
